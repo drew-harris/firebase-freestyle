5d18ffd6124bb0567f6926948cd6da870955dfab
Drew Harris <dsharris10@gmail.com>
2022-07-29 11:10:55 -0500

    Initial Commit

---
+++ FirebaseFreestyle/FirebaseFreestyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseFreestyleApp.swift
+//  FirebaseFreestyle
+//
+//  Created by Drew Harris on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseFreestyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
